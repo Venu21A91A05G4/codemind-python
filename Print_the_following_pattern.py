47cf103e18882a1d04383f77b476b3d89f091ff5
Venu21A91A05G4 <97424426+Venu21A91A05G4@users.noreply.github.com>
2022-06-30 11:33:15 +0530

    a=int(input()) arr=list("ABCDEFGHIJKLMNOPQRSTUVWXYZ") for i in range(a):     for j in range(a-(i+1)):         print(" ",end="")     for j in range(i+1):         print(arr[j],end="")     for j in range(i-1,-1,-1):         print(arr[j],end="")     print(""),

--- Print_the_following_pattern.py
+++ Print_the_following_pattern.py
@@ -1,5 +1,10 @@
-n=int(input())
-for i in range(1,n+1):
-    for j in range(i,n+1):
-           print((j),end=' ')
-    print()
+a=int(input())
+arr=list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+for i in range(a):
+    for j in range(a-(i+1)):
+        print(" ",end="")
+    for j in range(i+1):
+        print(arr[j],end="")
+    for j in range(i-1,-1,-1):
+        print(arr[j],end="")
+    print(""),
